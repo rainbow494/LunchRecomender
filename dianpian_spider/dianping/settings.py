a60df151425f29f126225d28ebca840ed8236c64
paul.huang <paul.huang@transfinder.com>
2018-07-15 10:07:08 +0800

    feat(spider):build scaffold

--- dianpian_spider/dianping/settings.py
+++ dianpian_spider/dianping/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'dianping.spiders'
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -70,16 +70,16 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- dianpian_spider/dianping/spiders/dianping_spider.py
+++ dianpian_spider/dianping/spiders/dianping_spider.py
@@ -18,8 +18,9 @@ class DianpingSpider(CrawlSpider):
     start_urls = [
         "http://www.ruanyifeng.com/blog/2004/01/",
     ]
-    rules={
-        Rule(LinkExtractor(allow='http://www.ruanyifeng.com/blog/2003/12/'), callback='parse_item')
+    rules = {
+        Rule(LinkExtractor(allow=r'2003\/12\/[\/_\w]*.html'), callback='parse_item'),
+        Rule(LinkExtractor(allow=r'2003\/12'), follow=True)
         # ,
         # Rule(LinkExtractor(allow='/2013/', restrict_css="ul li"), callback='parse_item')
     }
@@ -30,29 +31,10 @@ class DianpingSpider(CrawlSpider):
         print sys.stdout.encoding
 
         print sys.getdefaultencoding()
-        for sel in response.css('ul li'):
-
-            # title = sel.xpath('a/text()').extract_first()
-            # link = sel.xpath('a/@href').extract_first()
-            # desc = sel.xpath('text()').extract_first()
-            # logging.basicConfig(filename='f:/example.log', level=logging.DEBUG)
-            # logging.debug('111')
-            # logging.debug(title)
-            # logging.debug(link)
-            # logging.debug(desc)
-            # print '------------------------------------------------------'
-            # print desc
-            # print link
-            # print title
-            # print '------------------------------------------------------'
-
-            # filename = response.url.split("/")[-2]
-            # with open(filename, "wb") as f:
-            #     f.write(response.body)
-
+        for sel in response.css('article'):
             item = DianpingItem()
-            item['titleCSSSelector'] = sel.css('a').select('@href').extract_first()
-            item['title'] = sel.xpath('a/text()').extract_first()
-            item['link'] = sel.xpath('a/@href').extract_first()
-            item['desc'] = sel.xpath('text()').extract_first()
+            # item['titleCSSSelector'] = sel.css('a').select('@href').extract_first()
+            item['title'] = sel.xpath('h1/text()').extract_first()
+            item['link'] = '' #sel.xpath('a/@href').extract_first()
+            item['desc'] = '' #sel.xpath('text()').extract_first()
             yield item
